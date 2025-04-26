68d46eedfb500f615959a8f538315050047bd4ca
Yurii Novikov <urichcoolx@gmail.com>
2025-04-27 01:26:42 +0200

    created-range-input

--- src/app/pages/main/components/password-generator-form/password-generator-form.component.ts
+++ src/app/pages/main/components/password-generator-form/password-generator-form.component.ts
@@ -9,9 +9,20 @@ import { Component, inject } from '@angular/core';
 })
 export class PasswordGeneratorFormComponent {
   passwordToCopy: string = '';
+  rangeValue: number = 4;
   clipboard = inject(Clipboard);
 
   copyToClipboard(text: string) {
     this.clipboard.copy(text);
+  }
+
+  onRangeInput(event: Event) {
+    const input = event.target as HTMLInputElement;
+    const value =
+      ((Number(input.value) - Number(input.min)) /
+        (Number(input.max) - Number(input.min))) *
+      100;
+    input.style.background = `linear-gradient(to right, #A4FFAF ${value}%, #18171F ${value}%)`;
+    this.rangeValue = Number(input.value);
   }
 }
