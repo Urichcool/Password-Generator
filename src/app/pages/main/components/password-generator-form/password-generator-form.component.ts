52d3a15cf1e7b5ff1297efcf0766eb3b9d4bc2fe
Yurii Novikov <urichcoolx@gmail.com>
2025-04-30 02:22:24 +0200

    developed-password-generating-logic

--- src/app/pages/main/components/password-generator-form/password-generator-form.component.ts
+++ src/app/pages/main/components/password-generator-form/password-generator-form.component.ts
@@ -1,24 +1,47 @@
 import { ClipboardModule, Clipboard } from '@angular/cdk/clipboard';
 import { Component, inject } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { GeneratePassword } from 'js-generate-password';
 
 @Component({
   selector: 'app-password-generator-form',
-  imports: [ClipboardModule],
+  imports: [ClipboardModule, ReactiveFormsModule],
   templateUrl: './password-generator-form.component.html',
   styleUrl: './password-generator-form.component.scss',
 })
 export class PasswordGeneratorFormComponent {
-  passwordToCopy: string = '';
-  rangeValue: number = 4;
+  generator = GeneratePassword;
+  passwordForm = new FormGroup({
+    passwordToCopy: new FormControl(''),
+    length: new FormControl(4),
+    uppercaseCheckbox: new FormControl(false),
+    lowercaseCheckbox: new FormControl(false),
+    numbersCheckbox: new FormControl(false),
+    symbolsCheckbox: new FormControl(false),
+  });
   clipboard = inject(Clipboard);
-  copied:boolean = false
+  copied: boolean = false;
 
   copyToClipboard(text: string) {
-    if(text){
+    if (text) {
       this.clipboard.copy(text);
       this.copied = true;
-      setTimeout(() => this.copied = false, 2000)
+      setTimeout(() => (this.copied = false), 2000);
     }
+  }
+
+  onSubmit() {
+    const password: string = this.generator({
+      length: this.passwordForm.value.length,
+      uppercase: this.passwordForm.value.uppercaseCheckbox,
+      lowercase: this.passwordForm.value.lowercaseCheckbox,
+      numbers: this.passwordForm.value.numbersCheckbox,
+      symbols: this.passwordForm.value.symbolsCheckbox,
+    });
+
+    this.passwordForm.patchValue({ passwordToCopy: password });
+
+    console.log(this.passwordForm.value);
   }
 
   onRangeInput(event: Event) {
@@ -28,6 +51,6 @@ export class PasswordGeneratorFormComponent {
         (Number(input.max) - Number(input.min))) *
       100;
     input.style.background = `linear-gradient(to right, #A4FFAF ${value}%, #18171F ${value}%)`;
-    this.rangeValue = Number(input.value);
+    this.passwordForm.value.length = Number(input.value);
   }
 }
