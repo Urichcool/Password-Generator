ecb49f9ccef0e5106382c4b4473f5a013e68f510
Yurii Novikov <urichcoolx@gmail.com>
2025-05-09 01:09:25 +0200

    created-password-strength-logic

--- src/app/pages/main/components/password-generator-form/password-generator-form.component.ts
+++ src/app/pages/main/components/password-generator-form/password-generator-form.component.ts
@@ -1,12 +1,7 @@
 import { ClipboardModule, Clipboard } from '@angular/cdk/clipboard';
 import { NgClass } from '@angular/common';
 import { Component, inject } from '@angular/core';
-import {
-  FormControl,
-  FormGroup,
-  ReactiveFormsModule,
-  Validators,
-} from '@angular/forms';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { GeneratePassword } from 'js-generate-password';
 
 @Component({
@@ -35,12 +30,18 @@ export class PasswordGeneratorFormComponent {
   clipboard = inject(Clipboard);
   copied: boolean = false;
 
+
+
   copyToClipboard(text: string) {
     if (text) {
       this.clipboard.copy(text);
       this.copied = true;
       setTimeout(() => (this.copied = false), 2000);
     }
+  }
+
+  onChange(){
+    this.passwordForm.patchValue({ passwordToCopy: "" });
   }
 
   onSubmit() {
@@ -63,5 +64,31 @@ export class PasswordGeneratorFormComponent {
       100;
     input.style.background = `linear-gradient(to right, #A4FFAF ${value}%, #18171F ${value}%)`;
     this.passwordForm.value.length = Number(input.value);
+  }
+
+  passwordStrengthNumber(values: {
+    uppercaseCheckbox: boolean | undefined;
+    lowercaseCheckbox: boolean | undefined;
+    numbersCheckbox: boolean | undefined;
+    symbolsCheckbox: boolean | undefined;
+  }) {
+    let counter: number = 0;
+    const valuesArray: (boolean | undefined)[] = Object.values(values);
+    for (let i = 0; i < valuesArray.length; i += 1) {
+      if (valuesArray[i]) {
+        counter += 1;
+      }
+    }
+    return counter;
+  }
+
+  passwordStrengthText(strength: number) {
+    if (strength >= 2 && strength < 4) {
+      return 'medium';
+    }
+    if (strength === 4) {
+      return 'strong';
+    }
+    return 'weak';
   }
 }
